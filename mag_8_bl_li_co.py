f3b9e28dc9dc3a5b10a8226cd508c46c7c6294db
preethi-123s <ps8619177@gmail.com>
2024-12-13 16:31:52 +0530

    Create mag_8_bl_li_co.py

---
+++ mag_8_bl_li_co.py
@@ -0,0 +1,15 @@
+import random
+responses = [
+     "It is certain",
+     "Replace hazy, try again",
+     "Don't count on it",
+     "Yes, definitely",
+     "My reply is no",
+     "Outlook not so good",
+     "signs point to yes"
+
+]
+
+print("Ask the Magic B Ball a question")
+input()
+print(random.choice(responses))
